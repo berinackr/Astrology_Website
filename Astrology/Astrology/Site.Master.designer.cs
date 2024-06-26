6457134d1551cbfc49c6d013eb8a9e0d7374f693
“berinackr” <bernacakir888@gmail.com>
2024-06-26 19:14:28 +0300

    birlestirildi

--- Astrology/Astrology/Site.Master.designer.cs
+++ Astrology/Astrology/Site.Master.designer.cs
@@ -15,13 +15,13 @@ namespace Astrology
     {
 
         /// <summary>
-        /// TitleContent control.
+        /// head control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder TitleContent;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
         /// form1 control.
@@ -33,12 +33,12 @@ namespace Astrology
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// MainContent control.
+        /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
